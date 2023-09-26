0aa857fcec6c67895fa5d062bc221d73d49c0bcb
akoudri <akoudri@cenotelie.fr>
2023-09-26 12:38:01 +0200

    Fixes database feeding

--- data/feed_with_csv.sql
+++ data/feed_with_csv.sql
@@ -1,6 +1,6 @@
 \COPY manufacturing.services FROM 'data/services.csv' WITH CSV HEADER;
 \COPY manufacturing.employees FROM 'data/employees.csv' WITH CSV HEADER;
 \COPY public.people FROM 'data/people.csv' WITH CSV HEADER;
-\COPY public.temperatures FROM 'data/people.csv' WITH CSV HEADER;
+\COPY public.temperatures FROM 'data/temperatures.csv' WITH CSV HEADER;
 \COPY public.titanic FROM 'data/titanic.csv' WITH CSV HEADER;
 
---
+++ init.sql
@@ -0,0 +1,8 @@
+\i data/sales_tables.sql;
+\i data/sales_data.sql;
+\i data/manufacturing.sql;
+\i data/people.sql;
+\i data/temperatures.sql;
+\i data/titanic.sql;
+\i data/feed_with_csv.sql;
+
